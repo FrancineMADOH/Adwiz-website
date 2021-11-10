2396b55c6aa2a15028322d10f6c796962c75c8a6
FrancineMADOH <francinemadoh@gmail.com>
2021-11-11 00:09:27 +0100

    setting up template engine and basic static assets services .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+const path = require('path')
 const express = require('express');
-const res = require('express/lib/response');
+const hbs = require('hbs')
 const port = process.env.PORT || 3000
 
 const app = express();
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname , '../views'))
+app.use(express.static(path.join(__dirname,'../public')))
+hbs.registerPartials(path.join('../views/partials'))
+
 
 //setting up the routes for adwiz.cm
-
 app.get('/', (req,res)=>{
-    res.send('hello home page ')
+    res.render('index', { title: "my title"})
 })
-
 app.get('/about', (req,res)=>{
     res.send('about adwiz')
 })
@@ -39,6 +43,10 @@ app.get('services/farmadwiz', (req,res)=>{
 
     res.send('farmadwiz le projet')
 
+})
+
+app.get('/404', (req,res)=>{
+    res.render('404')
 })
 
 
