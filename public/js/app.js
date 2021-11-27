cf82a994cee1c6c488d0755e879c7c00873f2413
FrancineMADOH <francinemadoh@gmail.com>
2021-11-27 04:02:43 +0100

    style the footer

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,15 @@
 let date = new Date().getFullYear()
 document.querySelector('.date').innerHTML = date;
 console.log(date);
+
+/*
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+*/
